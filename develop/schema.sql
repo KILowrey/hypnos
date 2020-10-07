9119a3cc8d60dea16a02c628df385886ece0b242
KIL Bismuth <kiralowrey@gmail.com>
2020-10-06 17:20:07 -0700

    updated shema

--- develop/schema.sql
+++ develop/schema.sql
@@ -1,1 +1,9 @@
+-- Development Schema --
+
+-- Get rid of old stuff when we update --
+DROP DATABASE IF EXISTS hypnos_dev_db;
+-- restart --
 CREATE DATABASE hypnos_dev_db;
+-- Use it --
+USE hypnos_dev_db;
+
