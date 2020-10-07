68aae6cf0ff06da83c6517114a58da229b922848
KIL Bismuth <kiralowrey@gmail.com>
2020-10-06 19:42:43 -0700

    got rid of old junk

--- models/userSettings.js
+++
@@ -1,7 +0,0 @@
-// User Settings
-module.exports = function(sequelize, DataTypes) {
-  const UserSettings = sequelize.define("UserSettings", {
-    //insert usersetting here
-  });
-  return UserSettings;
-};
--- models/userSleep.js
+++
@@ -1,7 +0,0 @@
-// User Sleep
-module.exports = function(sequelize, DataTypes) {
-  const UserSleep = sequelize.define("UserSleep", {
-    //insert usersleep here
-  });
-  return UserSleep;
-};
