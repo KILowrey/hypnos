9a5746f6e80d9ab7382267e95e7a8117982fea1b
Diogo Candido <diogocandido.s@gmail.com>
2020-10-13 21:05:17 -0700

    updated

--- public/javascript/onBoard.js
+++ public/javascript/onBoard.js
@@ -1,24 +1,56 @@
 $(document).ready(function() {
     console.log("hello")
 
-var ageForm = $(".ageForm");
-var ageInput = $(".age") 
-
-console.log("")
-
-ageForm.on("submit", function(event) {
-    event.preventDefault();
+    var ageForm = $(".ageForm");
+    var ageInput = $(".age");
     
-    for (var i= 0; i < ageInput.length; i++ ) {
-        if (ageInput[i].checked) {
-         var userAge = ageInput[i].val(),
-        }
+    
+    
+
+    ageForm.on("submit", function(event) {
+        event.preventDefault();
+        
+        for (var i= 0; i < ageInput.length; i++ ) {
+            if (ageInput[i].checked) {
+            var userAge = {}
+            userAge[ageInput[i].name] = ageInput[i].value //user data all info of survey
+            
+            selectedAge(userAge);
+            
+            }; 
+           
+        };
+    });
+    
+    function selectedAge(age){
+        console.log(age)
+        $.post("/api/profiles") // make sure which api 
+        .then(function(value) {
+            throw new Error('oh, no!');
+        })
+        .catch(function(e) {
+            console.error(e.message); 
+        })
+    
     };
-    
-selectedAge(userAge);
-    ageInput[i].val()
 
-    console.log("Her");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
